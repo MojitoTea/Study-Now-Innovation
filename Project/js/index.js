519ad1a91a22afc434c971b6e90198be0b44fad8
MojitoTea <39397005+MojitoTea@users.noreply.github.com>
2019-04-30 22:37:05 +0300

    Add files via upload

---
+++ Project/js/index.js
@@ -0,0 +1,19 @@
+$(document).on('click','.js-videoPoster',function(e) {
+  //отменяем стандартное действие button
+  e.preventDefault();
+  var poster = $(this);
+  // ищем родителя ближайшего по классу
+  var wrapper = poster.closest('.js-videoWrapper');
+  videoPlay(wrapper);
+});
+
+//вопроизводим видео, при этом скрывая постер
+function videoPlay(wrapper) {
+  var iframe = wrapper.find('.js-videoIframe');
+  // Берем ссылку видео из data
+  var src = iframe.data('src');
+  // скрываем постер
+  wrapper.addClass('videoWrapperActive');
+  // подставляем в src параметр из data
+  iframe.attr('src',src);
+}
